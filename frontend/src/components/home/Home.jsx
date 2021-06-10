64c70810dbc660114272b0d31dfefe16808d7aa8
Alexander kruger <alex.bigbox@gmail.com>
2021-06-10 17:15:32 -0300

    Cadastro de Usuários React com Bootstrap

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Main from '../template/Main';
+
+export default props =>
+    <Main 
+      icon="home" 
+      title="Início"
+      subtitle="Segundo projeto do ReactJS"
+    >
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className='mb-0'>
+            Sistema para exemplificar a construção de um cadastro desenvolvido em React JS
+        </p>
+    </Main>
